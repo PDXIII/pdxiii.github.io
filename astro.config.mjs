eef97c857278d76aeb8cacb0cf95809513cf185f
Peter Sekan <sekan.pete@gmail.com>
2023-05-10 13:53:21 +0200

    change astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pdxiii.github.io",
+  site: "http://petersekan.de",
   integrations: [
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
