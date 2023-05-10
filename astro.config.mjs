fea84025295683b7ffbcc0f473e2569ac04d80db
Peter Sekan <sekan.pete@gmail.com>
2023-05-10 15:36:55 +0200

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pdxiii.github.io",
+  site: "https://petersekan.de",
   integrations: [
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
